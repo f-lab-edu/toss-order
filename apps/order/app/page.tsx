50f61ff900965a355f537f757a6e85bbca0acfdc
Turbobot <turbobot@vercel.com>
2023-10-21 00:51:17 +0900

    chore: 프로젝트 초기 설정

---
+++ apps/order/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button, Header } from 'ui';
+
+const Page = () => (
+  <>
+    <Header text="Toss Order" />
+    <Button />
+  </>
+);
+
+export default Page;
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,6 +1,33 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: [
+    "next",
+    "turbo",
+    "airbnb",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint", "import"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "no-console": [
+      "error",
+      {
+        allow: ["error"],
+      },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [
+      1,
+      {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    ],
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: "arrow-function",
+      },
+    ],
   },
+  ignorePatterns: ["node_modules/", "dist/"],
 };
