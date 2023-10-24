f512fc28a5c11cf1d0aed37a570d2312c1c74232
Yoonkeee <yoonkeee@gmail.com>
2023-10-24 16:06:17 +0900

    chore: eslint 세팅 수정

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,33 +1,31 @@
 module.exports = {
-  extends: [
-    "next",
-    "turbo",
-    "airbnb",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
-  plugins: ["@typescript-eslint", "import"],
+  extends: ['next', 'airbnb', 'airbnb-typescript', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'import'],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "no-console": [
-      "error",
+    '@next/next/no-html-link-for-pages': 'off',
+    'no-console': [
+      'error',
       {
-        allow: ["error"],
+        allow: ['error'],
       },
     ],
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-filename-extension": [
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
       1,
       {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     ],
-    "react/function-component-definition": [
+    'react/function-component-definition': [
       2,
       {
-        namedComponents: "arrow-function",
+        namedComponents: 'arrow-function',
       },
     ],
   },
-  ignorePatterns: ["node_modules/", "dist/"],
+  ignorePatterns: ['node_modules/', 'dist/'],
 };
