7d3d4cfdbd2a087603d617eb7ab5be2634d6fe16
Yoonkeee <yoonkeee@gmail.com>
2023-11-02 20:38:35 +0900

    chore: <Providers> 위치를 page에서 layout으로 이동

--- apps/order/app/layout.tsx
+++ apps/order/app/layout.tsx
@@ -1,3 +1,5 @@
+import Providers from './providers';
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
   <html lang="ko">
     <head>
@@ -6,7 +8,9 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => (
       <meta name="description" content="스마트폰으로 주문하는 테이블 오더 시스템입니다." />
       <title>토스 오더</title>
     </head>
-    <body>{children}</body>
+    <body>
+      <Providers>{children}</Providers>
+    </body>
   </html>
 );
 
