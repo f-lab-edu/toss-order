553622d28c5769fde8f52d2f768dd43dd6a88f1d
yoonki <yoonkeee@gmail.com>
2023-11-06 12:24:14 +0900

    feat: Firestore 세팅
    feat: Firestore 세팅

--- apps/order/app/firestore.ts
+++ apps/order/app/firestore.ts
@@ -1,17 +1,8 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: process.env['API-KEY'],
-  authDomain: process.env['AUTH-DOMAIN'],
-  projectId: process.env['PROJECT-ID'],
-  storageBucket: process.env['STORAGE-BUCKET'],
-  messagingSenderId: process.env['MESSAGING-SENDER-ID'],
-  appId: process.env['APP-ID'],
-  measurementId: process.env['MEASUREMENT-ID'],
-};
-
-const app = initializeApp(firebaseConfig);
+const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIRESTORE_ENV);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const firestore = getFirestore(app);
 
 export default firestore;
