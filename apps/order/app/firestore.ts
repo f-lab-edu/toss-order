ad893b010a163cab5bfe8eb505902eba24fb957e
Yoonkeee <yoonkeee@gmail.com>
2023-11-06 10:48:44 +0900

    feat: Firestore 세팅

--- apps/order/app/firestore.ts
+++ apps/order/app/firestore.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyB8snU4VKsKTlPI9b9Z7KWg6caDb5LyBIM',
-  authDomain: 'toss-order.firebaseapp.com',
-  projectId: 'toss-order',
-  storageBucket: 'toss-order.appspot.com',
-  messagingSenderId: '4103214725',
-  appId: '1:4103214725:web:686398096f9aad3e087bec',
-  measurementId: 'G-3RHXMZ43HL',
+  apiKey: process.env['API-KEY'],
+  authDomain: process.env['AUTH-DOMAIN'],
+  projectId: process.env['PROJECT-ID'],
+  storageBucket: process.env['STORAGE-BUCKET'],
+  messagingSenderId: process.env['MESSAGING-SENDER-ID'],
+  appId: process.env['APP-ID'],
+  measurementId: process.env['MEASUREMENT-ID'],
 };
 
 const app = initializeApp(firebaseConfig);
