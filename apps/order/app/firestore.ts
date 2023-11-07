577820f2bacb7dfbe18b178d535c9315297846d9
Yoonkeee <yoonkeee@gmail.com>
2023-11-08 02:02:16 +0900

    feat: firestore의 config과 init을 서버 사이드에서 구성하도록 변경

--- apps/order/app/firestore.ts
+++ apps/order/app/firestore.ts
@@ -1,8 +1,27 @@
 import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIRESTORE_ENV);
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
-const firestore = getFirestore(app);
+export function getFirebaseConfig() {
+  const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId,
+  };
 
-export default firestore;
+  return firebaseConfig;
+}
+// const firebaseConfig = { firebaseConfig }
+// const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+// const firestore = getFirestore(app);
+const Firestore = () => {
+  const firebaseConfig = getFirebaseConfig();
+  const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+  const firestore = getFirestore(app);
+  return firestore;
+};
+
+export default Firestore;
