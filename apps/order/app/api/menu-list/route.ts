f2f6ad1c3df63bbe9fa988fc0ef9123166c5fcc6
Yoonkeee <yoonkeee@gmail.com>
2023-11-12 03:29:52 +0900

    feat: api/menu-list/route.ts에서 status 500 추가

--- apps/order/app/api/menu-list/route.ts
+++ apps/order/app/api/menu-list/route.ts
@@ -3,5 +3,7 @@ import menuData from './get-menu';
 
 export async function GET() {
   const menu = await menuData;
+  if (!menu || Object.keys(menu).length <= 0) return NextResponse.json(null, { status: 500 });
+
   return NextResponse.json(menu, { status: 200 });
 }
