87b3fa29b359febd4bca7b304a0752c8814ddb7e
Yoonkeee <yoonkeee@gmail.com>
2023-11-12 21:35:23 +0900

    feat: 변수명 수정 및 메뉴 데이터가 없음에 대한 status를 500에서 404로 수정

--- apps/order/app/api/menu-list/route.ts
+++ apps/order/app/api/menu-list/route.ts
@@ -1,9 +1,13 @@
 import { NextResponse } from 'next/server';
-import menuData from './get-menu';
+import getMenuItems from './get-menu-items';
+
+interface MenuList {
+  [key: string]: { detail: string; image: string; price: number };
+}
 
 export async function GET() {
-  const menu = await menuData;
-  if (!menu || Object.keys(menu).length <= 0) return NextResponse.json(null, { status: 500 });
+  const menuItems: MenuList | void = await getMenuItems;
+  if (!menuItems || Object.keys(menuItems).length <= 0) return NextResponse.json(null, { status: 404 });
 
-  return NextResponse.json(menu, { status: 200 });
+  return NextResponse.json(menuItems, { status: 200 });
 }
