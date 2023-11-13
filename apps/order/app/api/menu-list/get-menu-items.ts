57127d0f0dada21541466211bf6ac0d4a6f45fca
Yoonkeee <yoonkeee@gmail.com>
2023-11-13 14:09:37 +0900

    feat: InitFirestore 삭제, 코드 가독성 향상

--- apps/order/app/api/menu-list/get-menu-items.ts
+++ apps/order/app/api/menu-list/get-menu-items.ts
@@ -1,4 +1,3 @@
-import { InitFirestore } from '../../lib/firestore';
 import { fetchData } from '../../lib/utils';
 
 interface MenuList {
@@ -8,22 +7,20 @@ interface MenuList {
 interface MenuSequence {
   order: { asc: { [key: string]: string } };
 }
-
-InitFirestore();
-
 const getUnsortedMenuItems: Promise<MenuList> = fetchData('menu-list');
 const getMenuSequence: Promise<MenuSequence> = fetchData('menu-sequence');
 
 const getMenuItems: Promise<MenuList | void> = Promise.all([getUnsortedMenuItems, getMenuSequence])
-  .then(([unsortedMenuItems, menuOrderSequence]) =>
-    Object.values(menuOrderSequence.order.asc).reduce(
+  .then(([unsortedMenuItems, menuOrderSequence]) => {
+    const menuNames: string[] = Object.values({ ...menuOrderSequence.order.asc });
+    return menuNames.reduce(
       (acc, menuName: string) => ({
         ...acc,
         [menuName]: unsortedMenuItems[menuName],
       }),
       {},
-    ),
-  )
+    );
+  })
   .catch(err => {
     if (process.env.NODE_ENV === 'development') console.error(err);
   });
