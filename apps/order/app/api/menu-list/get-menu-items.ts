5c24ad68a78533e04ef89b525340147edadbd35a
Yoonkeee <yoonkeee@gmail.com>
2023-11-15 16:28:08 +0900

    chore: fetchData 실패시 return null하도록 수정, Data structuring TODO 기록

--- apps/order/app/api/menu-list/get-menu-items.ts
+++ apps/order/app/api/menu-list/get-menu-items.ts
@@ -9,9 +9,11 @@ interface MenuSequence {
 }
 const getUnsortedMenuItems: Promise<MenuList> = fetchData('menu-list');
 const getMenuSequence: Promise<MenuSequence> = fetchData('menu-sequence');
-const getMenuItems: Promise<MenuList | void> = Promise.all([getUnsortedMenuItems, getMenuSequence])
-  .then(([unsortedMenuItems, menuOrderSequence]) => {
-    const menuNames = Object.values({ ...menuOrderSequence.order.asc });
+
+const getMenuItems: Promise<MenuList | null> = Promise.all([getUnsortedMenuItems, getMenuSequence])
+  .then(([unsortedMenuItems, { order }]) => {
+    const menuNames: string[] = Object.values(order.asc);
+    // TODO: Make data structuring. Ticket: toss-order #11
     return menuNames.reduce(
       (acc, menuName: string) => ({
         ...acc,
@@ -22,6 +24,7 @@ const getMenuItems: Promise<MenuList | void> = Promise.all([getUnsortedMenuItems
   })
   .catch(err => {
     if (process.env.NODE_ENV === 'development') console.error(err);
+    return null;
   });
 
 export default getMenuItems;
