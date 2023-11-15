ce675e57f1886e9eb1aa5f399470babe06e2dd82
Yoonkeee <yoonkeee@gmail.com>
2023-11-15 16:55:05 +0900

    chore: CTAButton의 props type 수정, disabled 추가

--- packages/ui/Button/button.tsx
+++ packages/ui/Button/button.tsx
@@ -3,18 +3,20 @@ import { Button } from '@chakra-ui/react';
 
 interface CTAButtonProps {
   className: string;
-  content: string | number | JSX.Element;
+  content: JSX.Element;
+  disabled: boolean;
   onClick?: () => void;
 }
 
-export const CTAButton = ({ className, content, onClick }: CTAButtonProps): JSX.Element => (
+export const CTAButton = ({ className, content, disabled = false, onClick }: CTAButtonProps): JSX.Element => (
   <Button
-    className={className}
     boxShadow="xl"
+    className={className}
     color="white"
+    disabled={disabled}
+    h="100%"
     onClick={onClick}
     w="100%"
-    h="100%"
     _hover={{ bgColor: null }}
   >
     {content}
