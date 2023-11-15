3a54dc9c63cb10fcf224ac640a5d5f9ca40673b2
Yoonkeee <yoonkeee@gmail.com>
2023-11-15 16:28:48 +0900

    chore: 실패한 경우와 데이터가 없는 경우를 분리하여 response와 메세지 응답하도록 수정

--- apps/order/app/api/menu-list/route.ts
+++ apps/order/app/api/menu-list/route.ts
@@ -6,8 +6,10 @@ interface MenuList {
 }
 
 export async function GET() {
-  const menuItems: MenuList | void = await getMenuItems;
-  if (!menuItems || Object.keys(menuItems).length <= 0) return NextResponse.json(null, { status: 404 });
+  const menuItems: MenuList | null = await getMenuItems;
+  if (!menuItems) return NextResponse.json({ message: 'failed to get data from firestore.' }, { status: 404 });
+
+  if (Object.keys(menuItems).length <= 0) return NextResponse.json({ message: 'data is empty' }, { status: 200 });
 
   return NextResponse.json(menuItems, { status: 200 });
 }
