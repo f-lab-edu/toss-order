9f3cc2df387fb2e4fbd2e33522f4a3678e69ca42
Yoonkeee <yoonkeee@gmail.com>
2023-11-15 19:44:17 +0900

    chore: context-provider -> ui-provider로 rename

--- packages/ui/ui-provider/index.tsx
+++ packages/ui/ui-provider/index.tsx
@@ -3,10 +3,10 @@
 import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider } from '@chakra-ui/react';
 
-const LayoutProvider = ({ children }: { children: React.ReactNode }) => (
+const UIProvider = ({ children }: { children: React.ReactNode }) => (
   <CacheProvider>
     <ChakraProvider resetCSS>{children}</ChakraProvider>
   </CacheProvider>
 );
 
-export default LayoutProvider;
+export default UIProvider;
