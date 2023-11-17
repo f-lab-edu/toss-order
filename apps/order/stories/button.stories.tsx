a0e59df068579350a1374bd97291ac9eb11dada4
Yoonkeee <yoonkeee@gmail.com>
2023-11-18 00:05:34 +0900

    feat: button.stories.tsx의 구조 수정

--- apps/order/stories/button.stories.tsx
+++ apps/order/stories/button.stories.tsx
@@ -1,35 +1,29 @@
-import { Meta } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 import { CTAButton } from 'ui/button';
 
-const buttonStoriesMeta: Meta = {
+const meta: Meta<typeof CTAButton> = {
   title: 'CTA Button',
+  component: CTAButton,
   argTypes: {
     content: {
       control: { type: 'text' },
-      defaultValue: <>Do Something</>,
       description: '버튼 내부에 들어갈 내용',
     },
     className: {
       control: { type: 'radio' },
       options: ['bg-blue', 'bg-orange', 'bg-pink'],
-      defaultValue: 'bg-blue',
       description: '버튼의 배경 색상',
     },
   },
+  tags: ['autodocs'],
 };
+export default meta;
 
-const createButtonStory = (ButtonComponent: React.ElementType) => {
-  const ButtonTemplate = ({ className, content }: { className: string; content: JSX.Element }) => (
-    <ButtonComponent content={content} className={className} />
-  );
-  const Story = ButtonTemplate.bind({});
-  Story.args = {
+type Story = StoryObj<typeof CTAButton>;
+
+export const CTAButtonStory: Story = {
+  args: {
     content: <>Do Something</>,
     className: 'bg-blue',
-  };
-
-  return Story;
+  },
 };
-
-export default buttonStoriesMeta;
-export const CTAButtonStory = createButtonStory(CTAButton);
