9cc22c9d6a3fc1c84b508540f08ea8738f5ecda3
Yoonkeee <yoonkeee@gmail.com>
2023-11-18 00:18:04 +0900

    feat: 헤더 추가

--- apps/order/app/page.tsx
+++ apps/order/app/page.tsx
@@ -1,7 +1,12 @@
 'use client';
 
-import Root from '../components/Root';
+import { Box } from '@chakra-ui/react';
+import { Header } from '../components/header';
 
-const Page: () => JSX.Element = () => <Root />;
+const Page: () => JSX.Element = () => (
+  <Box position="relative">
+    <Header />
+  </Box>
+);
 
 export default Page;
