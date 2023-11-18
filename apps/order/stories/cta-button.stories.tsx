90e651164934289f600268706e44a6b6e4a2072c
Yoonkeee <yoonkeee@gmail.com>
2023-11-19 01:43:12 +0900

    [DON'T PUSH] cta-button story

---
+++ apps/order/stories/cta-button.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { CTAButton } from 'ui/button';
+
+const meta: Meta<typeof CTAButton> = {
+  title: 'CTA Button',
+  component: CTAButton,
+  argTypes: {
+    content: {
+      control: false,
+      description: '버튼 내부에 들어갈 내용',
+    },
+    className: {
+      control: { type: 'radio' },
+      options: ['bg-blue', 'bg-orange', 'bg-pink'],
+      description: '버튼의 배경 색상',
+    },
+  },
+  tags: ['autodocs'],
+};
+export default meta;
+
+type Story = StoryObj<typeof CTAButton>;
+
+export const CTAButtonStory: Story = {
+  args: {
+    content: <>Do Something</>,
+    className: 'bg-blue',
+  },
+};
