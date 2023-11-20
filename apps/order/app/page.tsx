f706888b351c7bc7292f0768d6fa53e3e60a0df1
Yoonkeee <yoonkeee@gmail.com>
2023-11-20 21:16:53 +0900

    Root 추가

--- apps/order/app/page.tsx
+++ apps/order/app/page.tsx
@@ -2,10 +2,12 @@
 
 import { Box } from '@chakra-ui/react';
 import { Header } from '../components/header';
+import Root from '../components/Root';
 
 const Page: () => JSX.Element = () => (
   <Box position="relative">
     <Header />
+    <Root />
   </Box>
 );
 
