567e7138371aa000e20aff446b354d474e00bd36
Yoonkeee <yoonkeee@gmail.com>
2023-11-20 21:19:02 +0900

    MenuContainer

--- apps/order/components/MenuContainer.tsx
+++ apps/order/components/MenuContainer.tsx
@@ -1,7 +1,7 @@
 import { VStack } from '@chakra-ui/react';
 
 const MenuContainer = ({ children }: { children: React.ReactNode }) => (
-  <VStack p="2%" alignItems="center" justifyContent="center">
+  <VStack px="2%" alignItems="center" justifyContent="center">
     {children}
   </VStack>
 );
