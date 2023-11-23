3015c4c8c2391f450d649888559b349a42361703
Yoonkeee <yoonkeee@gmail.com>
2023-11-23 18:24:54 +0900

    feat: inner-text 삭제

--- apps/order/components/inner-text/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Text } from '@chakra-ui/react';
-
-export const InnerText = ({ children }: { children: string | number }): JSX.Element => (
-  <Text fontWeight="extrabold" fontSize="xl">
-    {children}
-  </Text>
-);
