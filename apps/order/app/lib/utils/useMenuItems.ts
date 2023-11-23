cf48197c529fe14685b0cf142c9eaa86fe35ba53
Yoonkeee <yoonkeee@gmail.com>
2023-11-23 18:25:40 +0900

    feat: axios로 data 할당하는 fetcher 추가

--- apps/order/app/lib/utils/useMenuItems.ts
+++ apps/order/app/lib/utils/useMenuItems.ts
@@ -1,8 +1,8 @@
 import { useQuery } from 'react-query';
-import axios from 'axios';
+import { fetcher } from './fetcher';
 
 const refineMenuItems = async () => {
-  const menuItems = await axios.get('/api/menu-list').then(({ data }) => data);
+  const menuItems = await fetcher('/api/menu-list');
   return Object.entries(menuItems).reduce((acc, [key, { image, ...rest }]) => {
     acc[key] = { imageSrc: image, ...rest };
     return acc;
