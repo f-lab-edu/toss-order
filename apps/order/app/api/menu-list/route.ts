ee944ff3eec490067b1e52b23e5e95eb1b243e0a
Yoonkeee <yoonkeee@gmail.com>
2023-11-24 01:16:44 +0900

    feat: price type 수정

--- apps/order/app/api/menu-list/route.ts
+++ apps/order/app/api/menu-list/route.ts
@@ -1,12 +1,12 @@
 import { NextResponse } from 'next/server';
 import getMenuItems from './get-menu-items';
 
-interface MenuList {
-  [key: string]: { name: string; detail: string; image: string; price: number };
-}
+type MenuItemsT = {
+  [key: string]: { name: string; detail: string; image: string; price: { [key: string]: number } };
+};
 
 export async function GET() {
-  const menuItems: MenuList | null = await getMenuItems();
+  const menuItems: MenuItemsT | null = await getMenuItems();
   if (!menuItems) return NextResponse.json({ message: 'failed to get data from firestore.' }, { status: 404 });
 
   if (Object.keys(menuItems).length <= 0) return NextResponse.json({ message: 'data is empty' }, { status: 200 });
