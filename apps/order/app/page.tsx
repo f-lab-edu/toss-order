8c9321c79f543fed370b9577b2b8314f33a99ef2
Yoonkeee <yoonkeee@gmail.com>
2023-11-25 17:59:02 +0900

    feat: page에 Suspense, fallback skeleton 추가

--- apps/order/app/page.tsx
+++ apps/order/app/page.tsx
@@ -1,7 +1,13 @@
 'use client';
 
+import { Suspense } from 'react';
 import { MenuContainer } from '../components/menu';
+import { MenuSkeleton } from '../components/menu/skeleton';
 
-const Page: () => JSX.Element = () => <MenuContainer />;
+const Page: () => JSX.Element = () => (
+  <Suspense fallback={<MenuSkeleton />}>
+    <MenuContainer />
+  </Suspense>
+);
 
 export default Page;
