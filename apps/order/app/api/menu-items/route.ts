fbcb88fd4652857172224241f4dae65c631f09aa
Yoonkeee <yoonkeee@gmail.com>
2023-11-27 02:08:43 +0900

    기타

--- apps/order/app/api/menu-items/route.ts
+++ apps/order/app/api/menu-items/route.ts
@@ -7,7 +7,7 @@ type MenuItemsT = {
 
 export async function GET() {
   const menuItems: MenuItemsT | null = await getMenuItems();
-  if (menuItems === null) return NextResponse.json({ message: 'failed to get data from database.' }, { status: 404 });
+  if (menuItems === null) return NextResponse.json({ message: 'failed to get data from database.' }, { status: 503 });
 
   if (Object.keys(menuItems).length <= 0) return NextResponse.json({ message: 'data is empty' }, { status: 200 });
 
