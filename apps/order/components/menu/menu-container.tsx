88503ac5c5944ba8a040267d0cb343db73f441b8
Yoonkeee <yoonkeee@gmail.com>
2023-11-27 02:15:41 +0900

    feat: state handling logic을 recoil에서 dispatchAction으로 이동

--- apps/order/components/menu/menu-container.tsx
+++ apps/order/components/menu/menu-container.tsx
@@ -1,61 +1,35 @@
-import { Stack, VStack, Box } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
-import { useRecoilState } from 'recoil';
-import _ from 'lodash';
+import { Stack, VStack } from '@chakra-ui/react';
+import { SetterOrUpdater, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { Menu } from './menu';
-import { basketItemsStore } from '../../app/stores';
-import useMenuItems from '../../app/lib/utils/useMenuItems';
+import { basketItemsStore, menuIdsStore, menuItemsStore, updateBasketStore } from '../../app/stores';
+
+type UpdateBasketParameterT = {
+  amount: 1 | -1;
+  id: string;
+};
 
 const MenuContainer = () => {
-  const [menuIds, setMenuIds] = useState([]);
+  const menuIds = useRecoilValue(menuIdsStore);
+
+  const menuItems = useRecoilValue(menuItemsStore);
 
   const [basketItems, setBasketItems] = useRecoilState(basketItemsStore);
 
-  const { data } = useMenuItems();
+  const updateBasket: SetterOrUpdater<UpdateBasketParameterT> = useSetRecoilState(updateBasketStore);
 
-  const resetBasket = () => {
-    setBasketItems(() => {
-      const newBasket = _.cloneDeep(data);
-      Object.keys(newBasket).forEach(key => {
-        newBasket[key].count = 0;
-        newBasket[key].totalPrice = 0;
-      });
-      return newBasket;
-    });
-  };
-
-  useEffect(() => {
-    if (data) {
-      setMenuIds(
-        Object.keys(data).reduce((acc, cur) => {
-          acc.push(cur);
-          return acc;
-        }, []),
-      );
-      if (Object.keys(basketItems).length === 0) resetBasket();
-    }
-  }, [data]);
+  const resetBasket = () => setBasketItems({});
 
   const getQuantity = (id: string) => basketItems[id]?.count ?? 0;
-
-  const changeQuantity = (id: string, amount: 1 | -1) => {
-    setBasketItems((currentBasket: object) => {
-      const newBasket = _.cloneDeep(currentBasket);
-      newBasket[id].count += amount;
-      newBasket[id].totalPrice += data[id].price.defaultPrice * amount;
-      return newBasket;
-    });
-  };
 
   return (
     <VStack alignItems="center" justifyContent="center" mb="1rem" px="4%">
       <Stack alignItems="center" w="100%">
-        {menuIds.map(id => (
+        {menuIds.map((id: string) => (
           <Menu key={id}>
-            <Menu.ItemArea {...data[id]} />
+            <Menu.ItemArea {...menuItems[id]} />
             <Menu.ButtonArea
               onClick={() => {
-                changeQuantity(id, 1);
+                updateBasket({ amount: 1, id });
               }}
               quantity={getQuantity(id)}
             />
