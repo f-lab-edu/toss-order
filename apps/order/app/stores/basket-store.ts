d499d8f53ac7aa567cce12f4aba0bc89c68ed64f
Yoonkeee <yoonkeee@gmail.com>
2023-11-27 02:05:09 +0900

    feat: basket-store에서 장바구니를 조작하는 dispatchAction 생성

---
+++ apps/order/app/stores/basket-store.ts
@@ -0,0 +1,68 @@
+import { atom, RecoilState, selector } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+import { menuItemsStore } from './menu-store';
+
+type MenuItemT = {
+  detail: string;
+  imageSrc: string;
+  name: string;
+  price: { defaultPrice: number };
+};
+
+type BasketItemT = {
+  count: number;
+  name: string;
+  totalPrice: number;
+};
+
+type BasketItemsT = {
+  [key: string]: BasketItemT;
+};
+
+type UpdateBasketParameterT = {
+  amount: 1 | -1;
+  id: string;
+};
+
+const { persistAtom } = recoilPersist();
+
+export const basketItemsStore: RecoilState<BasketItemsT> = atom({
+  key: 'basketItemsStore',
+  default: {},
+  effects_UNSTABLE: [persistAtom],
+});
+
+// @ts-ignore
+export const updateBasketStore = selector({
+  key: 'updateBasketStore',
+  get: () => {},
+  set: ({ set, get }, { amount, id }: UpdateBasketParameterT) => {
+    const menuItem: MenuItemT = get(menuItemsStore)[id];
+
+    const basket: BasketItemsT = get(basketItemsStore);
+
+    const itemInBasket: BasketItemT | undefined = basket[id];
+
+    const itemToBeUpdated: BasketItemT = {
+      count: 0,
+      name: '',
+      totalPrice: 0,
+    };
+
+    if (itemInBasket) {
+      itemToBeUpdated.count = itemInBasket.count;
+      itemToBeUpdated.name = itemInBasket.name;
+      itemToBeUpdated.totalPrice = itemInBasket.totalPrice;
+    } else {
+      itemToBeUpdated.name = menuItem.name;
+    }
+
+    itemToBeUpdated.count += amount;
+    itemToBeUpdated.totalPrice = menuItem.price.defaultPrice * itemToBeUpdated.count;
+
+    set(basketItemsStore, {
+      ...basket,
+      [id]: itemToBeUpdated,
+    });
+  },
+});
