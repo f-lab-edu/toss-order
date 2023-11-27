eb96fc4644c157ee0c5d19ec02652e1dcb6bbd7b
Yoonkeee <yoonkeee@gmail.com>
2023-11-27 23:18:26 +0900

    feat: useQuery의 return type 삭제

--- apps/order/app/lib/utils/useMenuItems.ts
+++ apps/order/app/lib/utils/useMenuItems.ts
@@ -1,4 +1,4 @@
-import { useQuery, UseQueryResult } from 'react-query';
+import { useQuery } from 'react-query';
 import { useSetRecoilState } from 'recoil';
 import { fetcher } from './fetcher';
 import { menuItemsStore } from '../../stores';
@@ -12,11 +12,11 @@ type MenuItemsT = {
   };
 };
 
-const useMenuItems: () => UseQueryResult<MenuItemsT | null> = () => {
+const useMenuItems = () => {
   const setMenuItems = useSetRecoilState(menuItemsStore);
   return useQuery('menu-items', () => fetcher('/api/menu-items'), {
     suspense: true,
-    onSuccess: data => setMenuItems(data),
+    onSuccess: (data: MenuItemsT) => setMenuItems(data),
   });
 };
 
