1f15903aa08049f7ca9c3f031b002a3582506838
Yoonkeee <yoonkeee@gmail.com>
2023-11-29 01:58:32 +0900

    number in circle

---
+++ packages/ui/number-in-circle/index.tsx
@@ -0,0 +1,20 @@
+import { Flex, Text } from '@chakra-ui/react';
+
+export const NumberInCircle = ({ number }: { number: number }) => (
+  <Flex aspectRatio={1} h="100%">
+    <Flex
+      alignItems="center"
+      aspectRatio={1}
+      bgColor="white"
+      color="black"
+      h="100%"
+      justifyContent="center"
+      p={0}
+      rounded="full"
+    >
+      <Text fontSize="lg" fontWeight="extrabold">
+        {number}
+      </Text>
+    </Flex>
+  </Flex>
+);
