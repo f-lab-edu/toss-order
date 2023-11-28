8eed1c109e4b3733b3a51982cc212fbf8989a7b3
Yoonkeee <yoonkeee@gmail.com>
2023-11-29 02:02:20 +0900

    NO - pretendard font

--- apps/order/app/layout.tsx
+++ apps/order/app/layout.tsx
@@ -8,6 +8,11 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => (
       <meta content="width=device-width, initial-scale=1.0" name="viewport" />
       <meta content="스마트폰으로 주문하는 테이블 오더 시스템입니다." name="description" />
       <title>토스 오더</title>
+      <link
+        as="style"
+        href="https://cdnjs.cloudflare.com/ajax/libs/pretendard/1.3.9/static/pretendard.min.css"
+        rel="stylesheet"
+      />
     </head>
     <body>
       <UIProvider>
