ba43e8c1b0623da712219432e427280504ebf51c
Yoonkeee <yoonkeee@gmail.com>
2023-11-30 00:38:08 +0900

    NO - 불필요한 파일들 삭제

--- apps/order/components/buttons/checkout/inner-content/action-dsecription.tsx
+++
@@ -1,9 +0,0 @@
-import { Flex, Text } from '@chakra-ui/react';
-
-export const ActionDsecription = ({ action }: { action: string }) => (
-  <Flex alignItems="center" flexBasis={0} flexGrow={1} justifyContent="center">
-    <Text fontSize="xl" fontWeight={900}>
-      {action}
-    </Text>
-  </Flex>
-);
