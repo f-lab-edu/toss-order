708213c2316f6347ca0bb93bc27e090559b20773
Yoonkeee <yoonkeee@gmail.com>
2023-11-30 21:32:20 +0900

    feat: 닫기 버튼을 표시할지 구분하는 displayCloseButton boolean 추가

--- packages/ui/abstract-modal/header.tsx
+++ packages/ui/abstract-modal/header.tsx
@@ -2,27 +2,30 @@ import { Flex, HStack, ModalHeader, Text } from '@chakra-ui/react';
 import { CTAButton } from '../button';
 
 type HeaderT = {
+  displayCloseButton: boolean;
   onClose: () => void;
   title: string;
 };
 
-export const Header = ({ onClose, title }: HeaderT) => (
+export const Header = ({ displayCloseButton = true, onClose, title }: HeaderT) => (
   <ModalHeader borderBottom="1px solid lightgray" flex="none" h="8vh" py={0}>
     <HStack h="100%" justifyContent="space-between" py={0} w="100%">
       <Text fontSize="2xl" fontWeight={900}>
         {title}
       </Text>
-      <Flex flexBasis="25%" h="50%">
-        <CTAButton
-          className="bg-pink"
-          content={
-            <Text fontSize="xl" fontWeight={800}>
-              닫기
-            </Text>
-          }
-          onClick={onClose}
-        />
-      </Flex>
+      {displayCloseButton ? (
+        <Flex flexBasis="25%" h="50%">
+          <CTAButton
+            className="bg-pink"
+            content={
+              <Text fontSize="xl" fontWeight={800}>
+                닫기
+              </Text>
+            }
+            onClick={onClose}
+          />
+        </Flex>
+      ) : null}
     </HStack>
   </ModalHeader>
 );
