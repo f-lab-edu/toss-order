154e24df34b5717030b9d26f632d32a8043278ff
Yoonkeee <yoonkeee@gmail.com>
2023-12-03 01:25:50 +0900

    푸터 수정

--- apps/order/components/footer/index.tsx
+++ apps/order/components/footer/index.tsx
@@ -4,36 +4,20 @@ import { useRecoilState } from 'recoil';
 import { basketItemsStore } from '../../app/stores';
 import { PrimaryCTAButton } from '../buttons/primary-cta-button';
 import { BasketModal } from '../modals/basket';
-import { ConfirmModal } from '../modals/confirm';
 
 export const Footer = () => {
   const [basketItems, setBasketItems] = useRecoilState(basketItemsStore);
   const { isOpen: isBasketOpen, onOpen: onBasketOpen, onClose: onBasketClose } = useDisclosure();
-  const { isOpen: isConfirmOpen, onOpen: onConfirmOpen, onClose: onConfirmClose } = useDisclosure();
-
-  const text = isBasketOpen ? '주문하기' : '장바구니 보기';
-
-  const toggleBasketAndConfirmModals = () => {
-    if (isBasketOpen) {
-      onBasketClose();
-      onConfirmOpen();
-    } else {
-      onBasketOpen();
-      onConfirmClose();
-    }
-  };
 
   useEffect(() => {
     if (basketItems.sumCount === 0) {
       setBasketItems({});
       onBasketClose();
-      onConfirmClose();
     }
   }, [basketItems.sumCount]);
 
   return (
     basketItems.sumCount > 0 && (
-      // return basketItems.sumCount > 0 && !isBasketOpen ? (
       <Flex
         alignItems="center"
         bgColor="white"
@@ -49,9 +33,9 @@ export const Footer = () => {
         <Box color="white" flex={0.7} h="100%" pb="10px" w="100%">
           <PrimaryCTAButton
             count={basketItems.sumCount}
-            onClick={toggleBasketAndConfirmModals}
+            onClick={onBasketOpen}
             price={basketItems.sumPrice}
-            text={text}
+            text="장바구니 보기"
           />
         </Box>
         <BasketModal isOpen={isBasketOpen} onClose={onBasketClose} />
