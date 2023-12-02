1af8734ad5a13f7b935cc4b6f9c5ca0095fb42a2
Yoonkeee <yoonkeee@gmail.com>
2023-12-02 12:31:21 +0900

    feat: Modal -> ChakraModal, AbstractModal -> Modal로 이름 수정

--- packages/ui/abstract-modal/index.tsx
+++ packages/ui/abstract-modal/index.tsx
@@ -1,4 +1,4 @@
-import { Modal } from '@chakra-ui/modal';
+import { Modal as ChakraModal } from '@chakra-ui/modal';
 import { ModalContent, ModalOverlay } from '@chakra-ui/react';
 import { ReactNode } from 'react';
 import { Header } from './header';
@@ -11,15 +11,15 @@ type ModalT = {
   onClose: () => void;
 };
 
-export const AbstractModal = ({ children, isOpen, onClose }: ModalT) => (
-  <Modal isCentered isOpen={isOpen} motionPreset="slideInBottom" onClose={onClose} scrollBehavior="inside">
+export const Modal = ({ children, isOpen, onClose }: ModalT) => (
+  <ChakraModal isCentered isOpen={isOpen} motionPreset="slideInBottom" onClose={onClose} scrollBehavior="inside">
     <ModalOverlay />
     <ModalContent borderRadius="3xl" maxW={document.body.clientWidth * 0.92}>
       {children}
     </ModalContent>
-  </Modal>
+  </ChakraModal>
 );
 
-AbstractModal.Header = Header;
-AbstractModal.Body = Body;
-AbstractModal.Footer = Footer;
+Modal.Header = Header;
+Modal.Body = Body;
+Modal.Footer = Footer;
