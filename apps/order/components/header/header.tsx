6fab97661dbb15aa6ebca07ce94949744982d765
Yoonkeee <yoonkeee@gmail.com>
2023-12-03 01:17:06 +0900

    feat: 헤더 컴포넌트 생성 (#33)
    * feat: 헤더 컴포넌트 생성
    
    * feat: 버튼의 width를 %에서 rem으로 수정
    
    * feat: 메뉴판 텍스트에 space 대신 letterSpacing 속성 적용
    
    * feat: 헤더의 font-size 속성을 반응형으로 수정, 버튼의 크기를 컨테이너에 의존하지 않게 수정

---
+++ apps/order/components/header/header.tsx
@@ -0,0 +1,26 @@
+import { HStack, Text } from '@chakra-ui/react';
+import { CallStaff } from './call-staff';
+import { OpenBill } from './open-bill';
+
+export const Header = () => (
+  <HStack
+    alignItems="center"
+    bgColor="white"
+    borderBottom="2px solid lightgray"
+    gap={0}
+    justifyContent="space-between"
+    minH="10vh"
+    pb="2vh"
+    position="sticky"
+    px="4%"
+    top={0}
+    w="100%"
+    zIndex={1}
+  >
+    <CallStaff />
+    <Text fontSize="calc(min(1.5rem + 1vw, 2rem))" fontWeight={900} letterSpacing="0.5rem">
+      메뉴판
+    </Text>
+    <OpenBill />
+  </HStack>
+);
