39e7e38f365d4f07215d64d3649e97a61e6cfb98
Yoonkeee <yoonkeee@gmail.com>
2023-12-03 21:13:48 +0900

    feat: Modal의 isOpen state를 저장하기 위한 store file 과 hook 생성

---
+++ apps/order/app/lib/utils/useStoredModalState.ts
@@ -0,0 +1,26 @@
+import { useRecoilState } from 'recoil';
+import { modalStateStore } from '../../stores/modal-state-store';
+
+type ModalNamesT = 'basket';
+
+export const useStoredModalState = (modalName: ModalNamesT) => {
+  const [isOpenStates, setIsOpenStates] = useRecoilState(modalStateStore);
+  const modalState = isOpenStates[modalName];
+  const { isOpen } = modalState;
+
+  const onOpen = () => {
+    setIsOpenStates(prevState => ({
+      ...prevState,
+      [modalName]: { ...prevState[modalName], isOpen: true },
+    }));
+  };
+
+  const onClose = () => {
+    setIsOpenStates(prevState => ({
+      ...prevState,
+      [modalName]: { ...prevState[modalName], isOpen: false },
+    }));
+  };
+
+  return { isOpen, onOpen, onClose };
+};
