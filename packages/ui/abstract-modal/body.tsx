6c382ef5095bac251c9b6f928b1fe25f0dcb64e4
Yoonkeee <yoonkeee@gmail.com>
2023-12-07 20:46:00 +0900

    modal body max height 수정

--- packages/ui/abstract-modal/body.tsx
+++ packages/ui/abstract-modal/body.tsx
@@ -1,7 +1,7 @@
 import { ModalBody } from '@chakra-ui/react';
 
 export const Body = ({ content }: { content: JSX.Element }) => (
-  <ModalBody maxH="50vh" p={0}>
+  <ModalBody maxH="40vh" p={0}>
     {content}
   </ModalBody>
 );
