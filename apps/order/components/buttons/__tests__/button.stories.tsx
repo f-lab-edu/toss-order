5aaa806c2980e4e4e46a90802c6f3e2d9ed93cfd
Yoonkeee <yoonkeee@gmail.com>
2023-12-10 00:39:13 +0900

    jest, storybook 의존성 및 관련 파일 삭제

--- apps/order/components/buttons/__tests__/button.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { Meta } from '@storybook/react';
-import { CallStaff, OpenBill } from '..';
-
-export default {
-  title: 'Components / Forms / Button',
-};
-export const CallStaffStory = () => <CallStaff />;
-export const OpenBillStory = () => <OpenBill />;
