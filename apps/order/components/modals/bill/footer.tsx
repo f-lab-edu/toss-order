07e915c38282d27b063d5261a0a2e94f4f0621ca
Yoonkeee <yoonkeee@gmail.com>
2023-12-09 23:54:41 +0900

    footer 여백 조정

--- apps/order/components/modals/bill/footer.tsx
+++ apps/order/components/modals/bill/footer.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
   const { sumPrice } = useRecoilValue(orderHistoryStore);
   return (
     <Flex borderTop="2px solid lightgray" h="0" justifyContent="center" mb="auto" minH="80px" w="100%">
-      <Flex alignItems="center" h="80%" justifyContent="space-between" px="10%" w="100%">
+      <Flex alignItems="center" h="100%" justifyContent="space-between" px="10%" w="100%">
         <Text fontSize="2xl" fontWeight={800}>
           합계
         </Text>
