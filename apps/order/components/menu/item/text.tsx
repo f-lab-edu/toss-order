c3d9f8ce90ba1e3b92198b268647693840f6d11b
Yoonkeee <yoonkeee@gmail.com>
2023-12-15 12:48:15 +0900

    feat: undefined가 잠깐이라도 표시되는 것 방지

--- apps/order/components/menu/item/text.tsx
+++ apps/order/components/menu/item/text.tsx
@@ -4,10 +4,10 @@ import { commaizeNumber } from '@toss/utils';
 export const Text = ({ name, price }: ItemTextT): JSX.Element => (
   <VStack alignItems="flex-start" ml="6%">
     <ChakraText fontSize="xl" fontWeight={900}>
-      {name}
+      {name ?? ''}
     </ChakraText>
     <ChakraText fontSize="md" fontWeight={500}>
-      {commaizeNumber(price?.defaultPrice)}원
+      {price ? commaizeNumber(price.defaultPrice) : 0}원
     </ChakraText>
   </VStack>
 );
